8229ec740f9acb23d5921a00635bc7e4a2e21b98
Mikhail Valerich <myx-myx@mail.ru>
2019-02-21 14:01:40 +0500

    старт

---
+++ 1/script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+
+// var number = 5;
+// var string = "hello, blyat!"
+// var obj = {}
+
+// console.log(string);
+
+// let persone = {
+//     name: "siski",
+//     age: "31",
+//     isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png', 'her.jpg', 'soleniy.gif'];
+// console.log(arr[1]);
+
+// alert("hello, bleat!");
+// let answer = confirm("siski?");
+// console.log(answer)
+
+// let answer = prompt("Школота", "мамку ебал");
+// console.log(typeof(arr));
+
+// let answer = confirm("siski?");
+
+// let answer = +prompt("nos?", "hui");
+// console.log (typeof(answer));
+// console.log("arr" + " - object")
+// console.log(4 + +" - object")
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// // console.log(++incr);
+// // console.log(--decr);  
+// console.log(incr++);
+// console.log(decr--);   
+
+// console.log(6%4);
+
+// let isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked || !isClose )
+var a = 1, b = 1, c, d;
+
+c = (2+ ++a); alert(c);
